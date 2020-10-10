a0a93c14ada4f5c630cdc79cc53dc996fbbdd28d
Ems737 <emanuel.simonb@gmail.com>
2020-10-10 00:04:07 -0300

    Agrego carpeta Extras

---
+++ Extras/creacionDW.sql
@@ -0,0 +1,99 @@
+/*Este script crea el datawarehouse a partir de la bd Intermedia*/
+
+drop table if exists dataW.dimTiempo;
+create table dataW.dimTiempo as
+select
+	(year(Precio.fecha)*10000 + month(Precio.fecha)*100 + day(Precio.fecha)) as idTiempo,
+    year(Precio.fecha) as anio,
+	(case 
+		when quarter(fecha)=1 or quarter(fecha)=2 then '1er Semestre'
+        when quarter(fecha)=3 or quarter(fecha)=4 then '2do Semestre'
+	end) as semestre,
+    (case 
+		when quarter(fecha)=1 then '1er Trimestre'
+		when quarter(fecha)=2 then '2do Trimestre'
+        when quarter(fecha)=3 then '3er Trimestre'
+        when quarter(fecha)=4 then '4to Trimestre'
+	end) as trimestre,
+    (case 
+		when month(fecha)=1 then 'Enero'
+        when month(fecha)=2 then 'Febrero'
+        when month(fecha)=3 then 'Marzo'
+        when month(fecha)=4 then 'Abril'
+        when month(fecha)=5 then 'Mayo'
+        when month(fecha)=6 then 'Junio'
+        when month(fecha)=7 then 'Julio'
+        when month(fecha)=8 then 'Agosto'
+        when month(fecha)=9 then 'Septiembre'
+        when month(fecha)=10 then 'Octubre'
+        when month(fecha)=11 then 'Noviembre'
+        when month(fecha)=12 then 'Diciembre'
+	end) as mes
+from 
+	Precio
+group by 
+	Precio.fecha
+order by
+	idTiempo; 
+
+ALTER TABLE `dataW`.`dimTiempo` 
+CHANGE COLUMN `idTiempo` `idTiempo` BIGINT NOT NULL ,
+ADD PRIMARY KEY (`idTiempo`);
+-- --------------------------------------------------------
+drop table if exists dataW.dimCombustible;
+create table dataW.dimCombustible as 
+select
+	Combustible.idCombustible as idCombustible,
+	Combustible.tipo as tipo
+from 
+	Combustible
+order by
+	Combustible.idCombustible,
+	Combustible.tipo;
+-- --------------------------------------------------------
+drop table if exists dataW.dimBandera;
+create table dataW.dimBandera as 
+select
+	Bandera.idBandera as idBandera,
+	Bandera.bandera as bandera
+from 
+	Bandera
+order by
+	Bandera.idBandera,
+	Bandera.bandera;
+-- --------------------------------------------------------
+drop table if exists dataW.dimUbicacion;
+create table dataW.dimUbicacion as 
+select
+	Ubicacion.idUbicacion as idUbicacion,
+	Ubicacion.provincia as provincia
+from 
+	Ubicacion
+order by
+	Ubicacion.idUbicacion,
+	Ubicacion.provincia;
+-- --------------------------------------------------------
+drop table if exists dataW.Preciofact;
+create table dataW.Preciofact as
+select
+	Combustible.idCombustible, Bandera.idBandera, Ubicacion.idUbicacion,
+	(year(Precio.fecha)*10000 + month(Precio.fecha)*100 + day(Precio.fecha)) as idTiempo,
+	Precio.precio_combustible,
+    Precio.precio_dolar,
+    truncate(Precio.precio_combustible - Precio.precio_dolar,2) as brecha_dolar_combustible
+from Precio 
+inner join Combustible on Precio.idCombustible = Combustible.idCombustible 
+inner join Bandera on Precio.idBandera = Bandera.idBandera
+inner join Ubicacion on Precio.idUbicacion = Ubicacion.idUbicacion 
+group by
+	Precio.fecha,
+    Combustible.idCombustible,
+    Bandera.idBandera,
+    Ubicacion.idUbicacion,
+    Precio.precio_combustible,
+    Precio.precio_dolar
+order by 
+	idTiempo, 
+    idCombustible,
+    idBandera,
+    idUbicacion;
